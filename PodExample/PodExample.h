dd35d9346ea1cc22b8fa78401b2f4aea8d760050
Antonhy <aretto@everis.com>
2019-07-02 14:04:38 -0500

    Initial project setup

---
+++ PodExample/PodExample.h
@@ -0,0 +1,19 @@
+//
+//  PodExample.h
+//  PodExample
+//
+//  Created by everis on 7/2/19.
+//  Copyright © 2019 everis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodExample.
+FOUNDATION_EXPORT double PodExampleVersionNumber;
+
+//! Project version string for PodExample.
+FOUNDATION_EXPORT const unsigned char PodExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodExample/PublicHeader.h>
+
+
